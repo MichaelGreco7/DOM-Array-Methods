27fda2a0148f775dee6fa205a0345714ae97bc46
Michael Greco <michael.greco07@gmail.com>
2020-04-13 11:43:03 -0400

    Show millionaries - filter()

--- app.js
+++ app.js
@@ -43,6 +43,13 @@ function sortByRichest() {
   updateDOM();
 }
 
+// Filter only millionaries
+function showMilli() {
+  data = data.filter((user) => user.money > 1000000);
+
+  updateDOM();
+}
+
 // Add new object to data array
 function addData(obj) {
   data.push(obj);
@@ -74,3 +81,4 @@ function formatMoney(number) {
 addUserBtn.addEventListener('click', getRandomUser);
 doubleBtn.addEventListener('click', doubleMoney);
 sortBtn.addEventListener('click', sortByRichest);
+showMillionairesBtn.addEventListener('click', showMilli);
